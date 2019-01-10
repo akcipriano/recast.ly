3b998d9836ff4fe32fd95f6f720ffa58d9d6ff21
Your Name <your_email@whatever.com>
2019-01-09 22:14:10 -0800

    Working on dynamic VideoList component

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,11 @@
+import exampleVideoData from './exampleVideoData.js';
 var VideoList = () => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <div><h5><em>videoListEntry</em> view goes here</h5>{exampleVideoData[0].snippet.title}</div>
+    <div><h5><em>videoListEntry</em> view goes here</h5>{exampleVideoData[1]}</div>
+    <div><h5><em>videoListEntry</em> view goes here</h5>{exampleVideoData[2]}</div>
+    <div><h5><em>videoListEntry</em> view goes here</h5>{exampleVideoData[3]}</div>
+    <div><h5><em>videoListEntry</em> view goes here</h5>{'Hello'}</div>
   </div>
 );
 
