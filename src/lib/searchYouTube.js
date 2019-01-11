33d4640e2f6cc980b07e57750c98ad6b65fc11a0
Alyssa Cipriano <alyssacipriano@gmail.com>
2019-01-11 12:26:28 -0800

    Complete creating reusable API helper

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,43 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: {
+      part: 'snippet',
+      q: options.query,
+      maxResults: options.max,
+      videoEmbeddable: true,
+      key: options.key,
+      type: 'video'
+    },
+    success: function (data) {
+      return callback(data.items);
+    }
+
+
+  });
+
 };
 
 export default searchYouTube;
+// buildApiRequest('GET',
+//                 '/youtube/v3/search',
+//                 {'maxResults': '25',
+//                  'part': 'snippet',
+//                  'q': 'surfing',
+//                  'type': ''});
+
+
+
+// readAll: function(successCB, errorCB = null) {
+//   $.ajax({
+//     url: Parse.server,
+//     type: 'GET',
+//     data: { order: '-createdAt' },
+//     contentType: 'application/json',
+//     success: successCB,
+//     error: errorCB || function(error) {
+//       console.error('chatterbox: Failed to fetch messages', error);
+//     }
+//   });
+// }
